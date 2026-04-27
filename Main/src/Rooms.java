fba629ec9599d920bd0daeff6b5ebfe75c2232bd
RaymondZeng <raymondzeng32@gmail.com>
2026-04-27 10:55:21 -0400

    Room Class

--- Main/src/Rooms.java
+++ Main/src/Rooms.java
@@ -1,2 +1,39 @@
+import java.util.*;
+
 public class Rooms {
+    private static HashMap<String, Boolean> Rooms = new HashMap<>();
+
+    public Rooms() {
+        for (int i = 0; i != 9; i++) {
+            for (int j = 0; j != 5; j++) {
+                for (int n = 0; n != 21; n++) {
+                    String Floor = String.valueOf(i);
+                    String Wing = GetWing(j);
+                    String Room = String.valueOf(n);
+                    Rooms.put(Floor + Wing + Room, true);
+                }
+            }
+        }
+        System.out.println(Rooms);
+    }
+
+    private static String GetWing(int j) {
+        if (j ==1 ) {
+            return "w";
+        } else if (j == 2) {
+            return "s";
+        } else if (j == 3) {
+            return "e";
+        } else {
+            return "n";
+        }
+    }
+
+    public boolean getAvailability(String room) {
+        return Rooms.get(room);
+    }
+
+    public void setTaken(String room) {
+        Rooms.replace(room, false);
+    }
 }
