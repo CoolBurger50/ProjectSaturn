4333a132718d89aa6c931f7a8b2b0cea7d33b2a7
DanielSitnikov <danielsemailtrust@gmail.com>
2026-04-27 11:02:08 -0400

    coded Students, Enrollments, and Assignments classes (left some todos as well)

--- Main/src/Assignments.java
+++ Main/src/Assignments.java
@@ -1,2 +1,38 @@
 public class Assignments {
+
+    private static int globalGradeID = 1;
+    private int localGradeID;
+    private int assignmentID;
+    private int studentID;
+    private int grade;
+
+    //TODO replace assignmentID with assignment object when the class is coded
+    public Assignments(Students student, int assignmentID, int grade){
+        this.studentID = student.getStudentID();
+        this.assignmentID = assignmentID;
+        this.grade = grade;
+        localGradeID = globalGradeID;
+        globalGradeID++;
+    }
+
+    public int getGradeID() {
+        return localGradeID;
+    }
+
+    public int getAssignmentID() {
+        return assignmentID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString(){
+        return "INSERT INTO Grades (assignment_id, student_id, grade) VALUES (" + assignmentID + ", " + studentID + ", " + grade + ")";
+    }
 }
--- Main/src/Enrollments.java
+++ Main/src/Enrollments.java
@@ -1,2 +1,24 @@
 public class Enrollments {
+
+    private int studentID;
+    private int sectionID;
+
+    //TODO try to put section object instead of an int id when section class is coded
+    public Enrollments(Students student, int sectionID){
+        this.studentID = student.getStudentID();
+        this.sectionID = sectionID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public int getSectionID() {
+        return sectionID;
+    }
+
+    @Override
+    public String toString(){
+        return "INSERT INTO Enrollments (student_id, section_id) VALUES (" + studentID + ", " + sectionID + ")";
+    }
 }
