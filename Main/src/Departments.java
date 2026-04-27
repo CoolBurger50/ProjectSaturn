8af4508c35407bf4675111eb4795edcfc59848f6
EllisKohn <efuhawfa@gmail.com>
2026-04-27 11:03:30 -0400

    Added Teachers and Departments classes

--- Main/src/Departments.java
+++ Main/src/Departments.java
@@ -1,2 +1,21 @@
 public class Departments {
+    private String departmentName;
+    private static int counter = 1;
+    private int departmentID;
+    public Departments (String departmentName){
+        this.departmentName = departmentName;
+        departmentID = counter++;
+    }
+
+    public int getDepartmentID() {
+        return departmentID;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+    @Override
+    public String toString (){
+        return "INSERT INTO Departments (department_name) VALUES " + getDepartmentName() + ";";
+    }
 }
--- Main/src/Teachers.java
+++ Main/src/Teachers.java
@@ -1,2 +1,33 @@
 public class Teachers {
+    private String firstName;
+    private String lastName;
+    private static int counter = 1;
+    private int teacherID;
+    private int departmentID;
+    public Teachers (String firstName, String lastName, int departmentID){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.departmentID = departmentID;
+        teacherID = counter++;
+    }
+
+    public int getDepartmentID() {
+        return departmentID;
+    }
+
+    public int getTeacherID() {
+        return teacherID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    @Override
+    public String toString (){
+        return "INSERT INTO Teachers (first_name, last_name, department_id) VALUES ('" + firstName + "', '" + lastName + "', " + departmentID + ");";
+    }
 }
