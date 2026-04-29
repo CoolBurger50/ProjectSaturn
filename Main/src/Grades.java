4b8f34b831c65b072c808280ce08125952d114c9
DanielSitnikov <danielsemailtrust@gmail.com>
2026-04-29 10:57:23 -0400

    Changed Students to automatically make names and grades, and make an arraylist of Student objects by calling the constructor, also made Grades and started Assignments

--- Main/src/Grades.java
+++ Main/src/Grades.java
@@ -1,2 +1,38 @@
 public class Grades {
+
+    private static int globalGradeID = 1;
+    private int localGradeID;
+    private int assignmentID;
+    private int studentID;
+    private int grade;
+
+    //TODO replace assignmentID with assignment object when the class is coded
+    public Grades(Students student, int assignmentID){
+        this.studentID = student.getStudentID();
+        this.assignmentID = assignmentID;
+        grade = (int) (Math.random() * 26) + 75;
+        localGradeID = globalGradeID;
+        globalGradeID++;
+    }
+
+    public int getGradeID() {
+        return localGradeID;
+    }
+
+    public int getAssignmentID() {
+        return assignmentID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    @Override
+    public String toString(){
+        return "INSERT INTO Grades (assignment_id, student_id, grade) VALUES (" + assignmentID + ", " + studentID + ", " + grade + ")";
+    }
 }
