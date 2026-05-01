41dd869d9a57d7d7013ea099feb6ca74e4340f33
RaymondZeng <raymondz66@nycstudents.net>
2026-05-01 11:45:12 -0400

    QOL

--- Main/src/Rooms.java
+++ Main/src/Rooms.java
@@ -1,9 +1,33 @@
 import java.util.*;
 
 public class Rooms {
-    private final HashMap<String, Boolean> Rooms = new HashMap<>();
+    private int Floor;
+    private String Wing;
+    private int Room;
+    private static int counter = 1;
+    private int RoomID;
+    private final HashMap<Rooms, Boolean> Rooms = new HashMap<>();
 
-    public Rooms() {
+    // Constructor Method
+    public Rooms(int Floor, String Wing, int Room) {
+        Floor = this.Floor;
+        Wing = this.Wing;
+        Room = this.Room;
+        RoomID = counter++;
+    }
+
+    // Getter Methods
+    public int getFloor() {return Floor;}
+
+    public String getWing() {return Wing;}
+
+    public int getRoom() {return Room;}
+
+    public static HashMap<Rooms, Boolean> getAllTeachers() {
+        return Rooms;
+    }
+
+    public G() {
         for (int i = 0; i != 9; i++) {
             for (int j = 0; j != 5; j++) {
                 for (int n = 0; n != 21; n++) {
