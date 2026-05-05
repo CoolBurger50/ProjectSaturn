e1b54fea4d1e7107624816b548c23de5c135d786
DanielSitnikov <danielsemailtrust@gmail.com>
2026-05-05 11:01:19 -0400

    coded Assignments class functionality, subject to change

--- Main/src/Assignments.java
+++ Main/src/Assignments.java
@@ -1,17 +1,55 @@
+import java.util.ArrayList;
+
 public class Assignments {
 
     private static int globalAssignmentID = 1;
     private int localAssignmentID;
-    private int sectionID;
+    private int courseID;
     private String assignmentName;
     private String assignmentType;
+    private static ArrayList<Assignments> allAssignments = new ArrayList<Assignments>();
 
-    public Assignments(int sectionID){
-        String[] assignmentTypes = {"Regents", "Elective", "AP"};
-        this.sectionID = sectionID;
-        assignmentType = assignmentTypes[(int) (Math.random() * 3)];
-
+    public Assignments(int courseID){
+        this.courseID = courseID;
         localAssignmentID = globalAssignmentID;
+        assignmentName = "assignment " + localAssignmentID;
+        if (localAssignmentID % 5 == 0){
+            assignmentType = "Major";
+        } else {
+            assignmentType = "Minor";
+        }
         globalAssignmentID++;
+    }
+
+    public int getAssignmentID() {
+        return localAssignmentID;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
+
+    public String getAssignmentType() {
+        return assignmentType;
+    }
+
+    public static ArrayList<Assignments> getAllAssignments() {
+        return allAssignments;
+    }
+
+    public static void generateAssignments(int amount, int courseID){
+        for (int i = 0; i < amount; i++){
+            Assignments assignment = new Assignments(courseID);
+            allAssignments.add(assignment);
+        }
+    }
+
+    @Override
+    public String toString(){
+        return assignmentName + " " + assignmentType;
     }
 }
