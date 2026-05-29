83f6c28b36c3ba8ab09d55686da42a65e9fba207
CoolBurger50 <raymondz66@nycstudents.net>
2026-05-29 12:18:51 -0400

    Update MasterRunner.sh

--- Main/MasterRunner.sh
+++ Main/MasterRunner.sh
@@ -6,9 +6,12 @@ user=raymondz66
 rm $path/sql/InsertStatements.sql
 touch $path/sql/InsertStatements.sql
 
-mysql -u $user -p -h 127.0.0.1 -D ${user}_db < /sql/CreateTables.sql
+insertsql=$path/sql/InsertStatements.sql
+createsql=$path/sql/CreateTables.sql
+
+mysql -u $user -p -h 127.0.0.1 -D ${user}_db < $createsql
 
 javac $path/src/*.java
 java -cp $path/src Main
 
-mysql -u $user -p -h 127.0.0.1 -D ${user}_db < /sql/InsertStatements.sql
+mysql -u $user -p -h 127.0.0.1 -D ${user}_db < $insertsql
