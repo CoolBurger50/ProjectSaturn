f37a33cfd43415d2e29f0e13e70a9d46fed45891
RaymondZeng <raymondz66@nycstudents.net>
2026-06-01 13:00:38 -0400

    Better MasterRunner.sh Made CreateTables.sql and DropTables.sql seperate

--- Main/MasterRunner.sh
+++ Main/MasterRunner.sh
@@ -6,12 +6,19 @@ path=/home/${user}/Projects/ProjectSaturn/Main
 rm $path/sql/InsertStatements.sql
 touch $path/sql/InsertStatements.sql
 
-insertsql=$path/sql/InsertStatements.sql
-createsql=$path/sql/CreateTables.sql
+rm $path/sql/Installation.sql
+touch $path/sql/Installation.sql
 
-mysql -u $user -p -h 127.0.0.1 -D ${user}_db < $createsql
+createsql=$path/sql/CreateTables.sql
+dropsql=$path/sql/DropTables.sql
+insertsql=$path/sql/InsertStatements.sql
+installationsql=$path/sql/Installation.sql
 
 javac $path/src/*.java
 java -cp $path/src Main
 
-mysql -u $user -p -h 127.0.0.1 -D ${user}_db < $insertsql
+cat $createsql >> $installationsql
+cat $dropsql >> $installationsql
+cat $insertsql >> $installationsql
+
+mysql -u $user -p -h 127.0.0.1 -D ${user}_db < $installationsql
