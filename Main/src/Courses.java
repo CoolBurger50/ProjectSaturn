a2683510893654218854e2412133d34cde583f18
DanielSitnikov <siuhsijdgf@gmail.com>
2026-06-08 10:02:52 -0400

    added course_type for courses

--- Main/src/Courses.java
+++ Main/src/Courses.java
@@ -2,13 +2,15 @@ import java.util.ArrayList;
 
 public class Courses {
     private final String courseName;
+    private final String courseType;
     private static int courseCounter = 1;
     private final int courseID;
     private final int departmentID;
     private static final ArrayList<Courses> allCourses = new ArrayList<>();
 
-    public Courses (String courseName, int departmentID){
+    public Courses (String courseName, int departmentID, String courseType){
         this.courseName = courseName;
+        this.courseType = courseType;
         this.departmentID = departmentID;
         courseID = courseCounter++;
     }
@@ -29,9 +31,22 @@ public class Courses {
         for (String subject : subjects) {
             int departmentID = Departments.getDepartmentID(subject);
             String path = basePath + subject + "/courses.txt";
-            ArrayList<String> teachersList = HelperMethods.getFileData(path);
-            for (String current : teachersList) {
-                Courses currentCourse = new Courses(current, departmentID);
+            ArrayList<String> courseList = HelperMethods.getFileData(path);
+            for (String current : courseList) {
+                String courseType = "";
+                if (current.indexOf("AP") != -1){
+                    courseType = "AP";
+                } else if (current.indexOf("Regents") != -1) {
+                    courseType = "Regents";
+                } else {
+                    int r = (int) (Math.random() * 2);
+                    if (r == 0){
+                        courseType = "Regents";
+                    } else {
+                        courseType = "Elective";
+                    }
+                }
+                Courses currentCourse = new Courses(current, departmentID, courseType);
                 allCourses.add(currentCourse);
             }
         }
@@ -39,12 +54,13 @@ public class Courses {
 
     public static String createInserts() {
         StringBuilder sb=new StringBuilder();
-        sb.append("INSERT INTO Courses (course_name, department_id) VALUES\n");
+        sb.append("INSERT INTO Courses (course_name, course_type, department_id) VALUES\n");
         int count=0;
         int total=allCourses.size();
         for(Courses course:allCourses) {
             sb.append("('")
-                    .append(course.courseName).append("', ")
+                    .append(course.courseName).append("', '")
+                    .append(course.courseType).append("', ")
                     .append(course.departmentID).append(")");
             count++;
 
